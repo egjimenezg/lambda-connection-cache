94c07869c851cb85d59d3edf492fbe1fee418dff
Gamaliel Jiménez <egjimenezg@gmail.com>
2020-06-04 21:43:24 -0500

    #1 updating go modules and connections cache

--- cache/connection.go
+++ cache/connection.go
@@ -5,31 +5,61 @@ import (
 )
 
 var (
-  once      sync.Once
-  connCache *ConnectionCache
+  once        sync.Once
+  connCache   *ConnectionCache
 )
 
 // Function that will be executed to get the connection pool
 type ConnectionFunction func(connectionString string) (interface{}, error)
 
+// Entry with channel
+type cacheEntry struct {
+  entry   connectionPool
+  ready   chan struct{}
+}
+
 // Result of connection 
-type connResult struct {
-  value   interface{}
-  err     error
+type connectionPool struct {
+  connection    interface{}
+  err           error
 }
 
 // ConnectionCache saves the created connection pools 
 type ConnectionCache struct {
-  cache   map[string]connResult
+  cache   map[string]*cacheEntry
+  mutex   sync.Mutex
 }
 
+// New creates a connection cache only once
 func New() *ConnectionCache {
   once.Do(func(){
     connCache = &ConnectionCache{
-      cache: make(map[string]connResult),
+      cache: make(map[string]*cacheEntry),
     }
   })
 
   return connCache
 }
 
+// Get obtains or create a connection pool
+func (connectionCache *ConnectionCache) Get(connectionString string, connectionFunction ConnectionFunction) (interface{}, error) {
+  connectionCache.mutex.Lock()
+  connEntry := connectionCache.cache[connectionString]
+
+  if connEntry == nil {
+    connEntry = &cacheEntry{ready: make(chan struct{})}
+
+    connectionCache.cache[connectionString] = connEntry
+    connectionCache.mutex.Unlock()
+
+    connEntry.entry.connection, connEntry.entry.err = connectionFunction(connectionString)
+
+    //Broadcast to the goroutines that the function was executed
+    close(connEntry.ready)
+  } else {
+    connectionCache.mutex.Unlock()
+    <-connEntry.ready
+  }
+
+  return connEntry.entry.connection, connEntry.entry.err
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/egjimenezg/lambda-connection-cache
 
 go 1.12
 
-require github.com/stretchr/testify v1.6.0
+require (
+	github.com/DATA-DOG/go-sqlmock v1.4.1
+	github.com/stretchr/testify v1.6.0
+)
